5224d9783c61f2bd25f044048bd8d1c660009dd4
Nhat0712 <7123ndn@gmail.com>
2024-07-02 00:02:11 +0700

    up

--- Buoi_28/todoList.js
+++ Buoi_28/todoList.js
@@ -3,19 +3,16 @@ const todoAdd = document.querySelector("#todo-add");
 const taskList = document.querySelector(".task-list");
 
 btnAdd.addEventListener("click", function () {
-  // khai báo
   var task = document.createElement("li");
   var contentTask = document.createElement("p");
   var fixTask = document.createElement("div");
   var btnUpdate = document.createElement("i");
   var btnDelete = document.createElement("i");
 
-  // thiết lập nội dung và thuộc tính
   btnUpdate.classList.add("fa-regular", "fa-pen-to-square", "update");
   btnDelete.classList.add("fa-solid", "fa-trash", "delete");
   var memo = todoAdd.value;
 
-  // gán nội dung và thuộc tính
   fixTask.appendChild(btnUpdate);
   fixTask.appendChild(btnDelete);
   fixTask.style.display = "flex";
@@ -24,26 +21,22 @@ btnAdd.addEventListener("click", function () {
   task.appendChild(fixTask);
   contentTask.textContent = todoAdd.value;
 
-  // kiểm tra chuỗi trống
   if (todoAdd.value.trim() === "") {
     alert("Vui lòng nhập lại công việc");
     todoAdd.value = "";
   } else {
     taskList.appendChild(task);
 
-    // nút Xóa
     btnDelete.addEventListener("click", function () {
       task.remove();
     });
 
-    // nút Sửa
     btnUpdate.addEventListener("click", function () {
       var contentUpdate = document.createElement("input");
       var btnAddUpdate = document.createElement("button");
       btnAddUpdate.textContent = "Add Task";
       contentUpdate.value = memo;
 
-      // gán nội dung và thuộc tính khi sửa
       task.appendChild(contentUpdate);
       task.appendChild(btnAddUpdate);
       contentTask.remove();
@@ -64,7 +57,6 @@ btnAdd.addEventListener("click", function () {
       });
     });
 
-    // reset input todo
     todoAdd.value = "";
   }
 });
